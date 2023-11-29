fda488ef48cc15a17d359f6aabe66670cb573b6a
DanielGaloIndio <106313908+DanielGaloIndio@users.noreply.github.com>
2023-11-28 22:23:38 -0300

    Update and rename index.html to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('meuFormulario').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Aqui você pode adicionar lógica para processar os dados do formulário
+    // Por exemplo, você pode acessar os valores dos campos usando document.getElementById
+    // e então enviar esses dados para um servidor usando AJAX ou fetch.
+    
+    alert('Formulário enviado com sucesso!');
+});
